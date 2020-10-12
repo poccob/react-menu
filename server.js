b4094bf18f1bd0c87257adb2e35984d0dc0bccfd
Alex <32238619+poccob@users.noreply.github.com>
2020-10-12 16:46:13 +0800

    update for deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
   static: './build',
 });
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.RORT || 3001;
 
 server.use(middlewares);
 server.use(router);
