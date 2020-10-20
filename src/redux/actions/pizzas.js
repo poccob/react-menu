e80fd4a9a65f1f935b93b060db5f34f6e769fafb
Alex <32238619+poccob@users.noreply.github.com>
2020-10-20 16:20:13 +0800

    add +/- 10 items to cart

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -13,7 +13,7 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
 
   axios
   .get(
-      `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${
+      `https://tortuga-menu.ru/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${
         sortBy.type
       }&_order=${sortBy.order}`,
     )
